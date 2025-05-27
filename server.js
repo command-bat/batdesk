f5ff061343027c3b8f7c57c40572a9a9267f4c40
command-bat <tiagolaure@gmail.com>
2025-05-27 14:30:41 -0300

    upate

--- server.js
+++ server.js
@@ -11,32 +11,33 @@ app.use(express.static('public'));
 let broadcaster;
 
 io.on('connection', (socket) => {
-  console.log('Novo cliente conectado');
+    console.log('Novo cliente conectado');
 
-  socket.on('broadcaster', () => {
-    broadcaster = socket.id;
-    socket.broadcast.emit('broadcaster');
-  });
+    socket.on('broadcaster', () => {
+        broadcaster = socket.id;
+        socket.broadcast.emit('broadcaster');
+    });
 
-  socket.on('watcher', () => {
-    socket.to(broadcaster).emit('watcher', socket.id);
-  });
+    socket.on('watcher', () => {
+        socket.to(broadcaster).emit('watcher', socket.id);
+    });
 
-  socket.on('offer', (id, message) => {
-    socket.to(id).emit('offer', socket.id, message);
-  });
+    socket.on('offer', (id, message) => {
+        socket.to(id).emit('offer', socket.id, message);
+    });
 
-  socket.on('answer', (id, message) => {
-    socket.to(id).emit('answer', socket.id, message);
-  });
+    socket.on('answer', (id, message) => {
+        socket.to(id).emit('answer', socket.id, message);
+    });
 
-  socket.on('candidate', (id, message) => {
-    socket.to(id).emit('candidate', socket.id, message);
-  });
+    socket.on('candidate', (id, message) => {
+        socket.to(id).emit('candidate', socket.id, message);
+    });
 
-  socket.on('disconnect', () => {
-    socket.to(broadcaster).emit('disconnectPeer', socket.id);
-  });
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado');
+        socket.to(broadcaster).emit('disconnectPeer', socket.id);
+    });
 });
 
 const PORT = process.env.PORT || 3000;
